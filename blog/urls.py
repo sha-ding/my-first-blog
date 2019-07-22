396b61d3709fadc09c238380996c6fe9be72b5f1
My First Git <sakurada.shading@gmail.com>
2019-07-22 23:56:13 +0900

    Django Girls Tutorial: Extensions #3

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/<pk>/hide/', views.post_hide, name='post_hide'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+	path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     path('all/', views.post_all_list, name='post_all_list'),
 ]
